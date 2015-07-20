7c666de56c47eac2d48a6d5fca166771032aec28
losmicar <losmi.todor@gmail.com>
2015-07-20 17:18:44 +0200

    Youtube add video fix

--- addYoutube.php
+++ addYoutube.php
@@ -4,19 +4,20 @@
 require_once('lib/api.php');
 
 try{
-	$api = new BridApi(array('auth_token'=>'ENTER YOUR AUTH CODE HERE'));
+	$api = new BridApi(array('auth_token'=>'a9554d8b9b279b8a461d9535c022ba9f0c650d08'));
 
 	$youTubeUrl = 'https://www.youtube.com/watch?v=xE_7I3O0Kfw';
 
-	$youtubeData = array('url'=> $youTubeUrl);
+	$youtubeData = array('url'=> $youTubeUrl,'return'=>'php');
 	//Prefetch YouTube data (title, tags, etc.)
 	$_data = $api->checkUrl($youtubeData,true);
 
+	//print_r($_data); die();
+
 	if(!isset($_data->error)){
 
-		$_data->mp4 = $youTubeUrl;	//required
 		$_data->channel_id = 52;	//required
-		$_data->partner_id = 1637;	//required
+		$_data->partner_id = 1792;	//required
 
 		print_r($api->addVideo((array)$_data));
 		?>
